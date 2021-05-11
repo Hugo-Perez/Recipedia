cb4bb053e11ba67e1c83ec40fa20ff48f80a3ae6
Hugo Perez <pehugo2@gmail.com>
2021-05-11 23:43:38 +0200

    Added more recipebook methods to API

--- backend/src/main/java/com/hpc/backend/controller/RecipeController.java
+++ backend/src/main/java/com/hpc/backend/controller/RecipeController.java
@@ -2,6 +2,7 @@ package com.hpc.backend.controller;
 
 import com.hpc.backend.config.services.UserDetailsImpl;
 import com.hpc.backend.model.ApiResponse;
+import com.hpc.backend.model.Recipe;
 import com.hpc.backend.model.RecipeBook;
 import com.hpc.backend.model.auth.User;
 import com.hpc.backend.repository.RecipeBookRepository;
@@ -26,16 +27,12 @@ public class RecipeController {
 
     @RequestMapping(value="/myRecipeBooks", method=RequestMethod.POST)
     public ResponseEntity<?> myRecipeBooks(Authentication authentication) {
-        if (authentication != null) {
+        if (authentication.isAuthenticated()) {
             UserDetailsImpl userDetails = (UserDetailsImpl)authentication.getPrincipal();
             Optional<User> user = userRepository.findByUsername(userDetails.getUsername());
 
-            if(user.isPresent()) {
-                List<RecipeBook> myRecipeBooks = recipeBookRepository.findByOwner(user.get());
-                return ResponseEntity.ok().body(myRecipeBooks);
-            } else {
-                return ResponseEntity.badRequest().body("No user found");
-            }
+            List<RecipeBook> myRecipeBooks = recipeBookRepository.findByOwner(user.get());
+            return ResponseEntity.ok().body(myRecipeBooks);
 
         } else {
             return ResponseEntity.badRequest().body("Unable to load recipes");
@@ -44,24 +41,61 @@ public class RecipeController {
 
     @RequestMapping(value="/newRecipeBook", method=RequestMethod.POST)
     public ResponseEntity<?> newRecipeBook(Authentication authentication, @RequestBody RecipeBook recipeBook) {
-        if (authentication != null) {
+        if (authentication.isAuthenticated()) {
             UserDetailsImpl userDetails = (UserDetailsImpl)authentication.getPrincipal();
             Optional<User> user = userRepository.findByUsername(userDetails.getUsername());
 
-            if(user.isPresent()) {
+            if (recipeBookRepository.existsByTitle(recipeBook.getTitle()))
+                return ResponseEntity.badRequest().body(new ApiResponse("A recipe book with this name already exists!"));
 
-                if (recipeBookRepository.existsByTitle(recipeBook.getTitle())) {
-                    return ResponseEntity.badRequest().body(new ApiResponse("A recipe book with this name already exists!"));
-                }
 
-                recipeBook.setAuthor(user.get().getUsername());
-                recipeBook.setOwner(user.get());
+            recipeBook.setAuthor(user.get().getUsername());
+            recipeBook.setOwner(user.get());
 
+            RecipeBook insertedBook = recipeBookRepository.save(recipeBook);
+            return ResponseEntity.ok(insertedBook);
+
+        } else {
+            return ResponseEntity.badRequest().body(new ApiResponse("User not found, try logging in again"));
+        }
+    }
+
+    @RequestMapping(value="/editRecipeBook", method=RequestMethod.PUT)
+    public ResponseEntity<?> editRecipeBook(Authentication authentication, @RequestBody RecipeBook recipeBook) {
+        if (authentication.isAuthenticated()) {
+            UserDetailsImpl userDetails = (UserDetailsImpl)authentication.getPrincipal();
+            Optional<User> user = userRepository.findByUsername(userDetails.getUsername());
+
+            RecipeBook originalRecipeBook = recipeBookRepository.findFirstByIdAndOwner(recipeBook.getId(), user.get());
+
+            if (originalRecipeBook != null) {
                 RecipeBook insertedBook = recipeBookRepository.save(recipeBook);
                 return ResponseEntity.ok(insertedBook);
             } else {
-                return ResponseEntity.badRequest().body(new ApiResponse("User not found, try logging in again"));
+                return ResponseEntity.badRequest().body(new ApiResponse("Original recipe book could not be found"));
             }
+
+        } else {
+            return ResponseEntity.badRequest().body(new ApiResponse("User not found, try logging in again"));
+        }
+    }
+
+    @RequestMapping(value="/deleteRecipeBook", method=RequestMethod.DELETE)
+    public ResponseEntity<?> deleteRecipeBook(Authentication authentication, @RequestParam long id) {
+        if (authentication.isAuthenticated()) {
+            UserDetailsImpl userDetails = (UserDetailsImpl)authentication.getPrincipal();
+            Optional<User> user = userRepository.findByUsername(userDetails.getUsername());
+
+
+            RecipeBook originalRecipeBook = recipeBookRepository.findFirstByIdAndOwner(id, user.get());
+
+            if (originalRecipeBook != null) {
+                recipeBookRepository.delete(originalRecipeBook);
+                return ResponseEntity.ok(new ApiResponse("Deleted book with id = " + id));
+            } else {
+                return ResponseEntity.badRequest().body(new ApiResponse("The RecipeBook you tried to delete could not be found"));
+            }
+
 
         } else {
             return ResponseEntity.badRequest().body(new ApiResponse("User not found, try logging in again"));
--- backend/src/main/java/com/hpc/backend/repository/RecipeBookRepository.java
+++ backend/src/main/java/com/hpc/backend/repository/RecipeBookRepository.java
@@ -7,9 +7,13 @@ import com.hpc.backend.model.auth.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RecipeBookRepository extends JpaRepository<RecipeBook, Long> {
+
     List<RecipeBook> findByOwner(User owner);
+
+    RecipeBook findFirstByIdAndOwner(Long id, User owner);
 
     boolean existsByTitle(String title);
 }
